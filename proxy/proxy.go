c26a215f5157f6342dec1c8fdabd1d3efeef2229
Yiwei Gong <imwithye@gmail.com>
2024-07-04 14:35:16 +0800

    add logger

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,16 +1,20 @@
 package proxy
 
 import (
+	"github.com/charmbracelet/log"
 	"github.com/gin-gonic/gin"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
 )
 
 type Proxy struct {
 	*gin.Engine
 	TargetURL *url.URL
+
+	logger *log.Logger
 }
 
 func NewProxy(target string) (*Proxy, error) {
@@ -20,7 +24,15 @@ func NewProxy(target string) (*Proxy, error) {
 	}
 
 	gin.SetMode(gin.ReleaseMode)
-	r := &Proxy{Engine: gin.New(), TargetURL: targetURL}
+	r := &Proxy{
+		Engine:    gin.New(),
+		TargetURL: targetURL,
+		logger: log.NewWithOptions(os.Stdout, log.Options{
+			ReportCaller:    false,
+			ReportTimestamp: true,
+			TimeFormat:      time.Kitchen,
+			Prefix:          "🔍 ",
+		})}
 	r.Use(gin.Recovery())
 	r.Any("/*proxyPath", r.ProxyHandlerFunc())
 
@@ -32,17 +44,21 @@ func (p *Proxy) ProxyHandlerFunc() gin.HandlerFunc {
 		proxyURL := p.TargetURL.ResolveReference(c.Request.URL)
 		req, err := http.NewRequest(c.Request.Method, proxyURL.String(), c.Request.Body)
 		if err != nil {
+			p.logger.Errorf("Failed to create proxy request: %v", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
 			return
 		}
 		req.Header = c.Request.Header
+
 		client := &http.Client{}
 		resp, err := client.Do(req)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send request"})
+			p.logger.Errorf("Failed to send proxy request: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send proxy request"})
 			return
 		}
-		log.Println("Request proxied to", proxyURL.String(), resp.StatusCode)
+
+		p.logger.Info("Request proxied", "url", proxyURL.String(), "statusCode", resp.StatusCode)
 		for key, values := range resp.Header {
 			for _, value := range values {
 				c.Writer.Header().Add(key, value)
@@ -51,18 +67,19 @@ func (p *Proxy) ProxyHandlerFunc() gin.HandlerFunc {
 		c.Status(resp.StatusCode)
 		_, err = io.Copy(c.Writer, resp.Body)
 		if err != nil {
-			log.Println("Failed to copy response body")
+			p.logger.Errorf("Failed to copy response body: %v", err)
 			return
 		}
+
 		err = resp.Body.Close()
 		if err != nil {
-			log.Println("Failed to close response body")
+			p.logger.Errorf("Failed to close response body: %v", err)
 			return
 		}
 	}
 }
 
 func (p *Proxy) Run(addr string) error {
-	log.Println("Listening and serving proxy on", addr)
+	p.logger.Infof("Listening and serving proxy on %s", addr)
 	return p.Engine.Run(addr)
 }
