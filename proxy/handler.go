efb7954445235ca975e8ed10db1cfdbfe18046de
Yiwei Gong <imwithye@gmail.com>
2024-07-04 16:12:37 +0800

    move handlers to handler.go file

---
+++ proxy/handler.go
@@ -0,0 +1,62 @@
+package proxy
+
+import (
+	"embed"
+	"github.com/gin-gonic/gin"
+	"io"
+	"io/fs"
+	"net/http"
+	"strings"
+)
+
+//go:embed _webui/dist
+var embedWebuiFS embed.FS
+
+func (p *Proxy) webuiHandler() gin.HandlerFunc {
+	subFS, _ := fs.Sub(embedWebuiFS, "_webui/dist")
+	return func(c *gin.Context) {
+		fp := strings.TrimPrefix(c.Request.URL.Path, "/")
+		req := c.Request.Clone(c.Request.Context())
+		_, err := subFS.Open(fp)
+		if err != nil {
+			req.URL.Path = "/"
+		}
+		fs := http.FileServer(http.FS(subFS))
+		fs.ServeHTTP(c.Writer, req)
+	}
+}
+
+func (p *Proxy) proxyHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		proxyURL := p.TargetURL.ResolveReference(c.Request.URL)
+		req, err := http.NewRequest(c.Request.Method, proxyURL.String(), c.Request.Body)
+		if err != nil {
+			p.logger.Errorf("Failed to create proxy request: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
+			return
+		}
+		req.Header = c.Request.Header
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			p.logger.Errorf("Failed to send proxy request: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send proxy request"})
+			return
+		}
+		defer resp.Body.Close()
+
+		p.logger.Info("Request proxied", "url", proxyURL.String(), "statusCode", resp.StatusCode)
+		for key, values := range resp.Header {
+			for _, value := range values {
+				c.Writer.Header().Add(key, value)
+			}
+		}
+		c.Status(resp.StatusCode)
+		_, err = io.Copy(c.Writer, resp.Body)
+		if err != nil {
+			p.logger.Errorf("Failed to copy response body: %v", err)
+			return
+		}
+	}
+}
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -3,7 +3,6 @@ package proxy
 import (
 	"github.com/charmbracelet/log"
 	"github.com/gin-gonic/gin"
-	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -39,46 +38,11 @@ func NewProxy(target string) (*Proxy, error) {
 	}
 
 	r.proxy.Use(gin.Recovery())
-	r.proxy.Any("/*proxyPath", r.ProxyHandlerFunc())
+	r.proxy.Any("/*proxyPath", r.proxyHandler())
 	r.webui.Use(gin.Recovery())
 	r.webui.GET("/", r.webuiHandler())
 
 	return r, nil
-}
-
-func (p *Proxy) ProxyHandlerFunc() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		proxyURL := p.TargetURL.ResolveReference(c.Request.URL)
-		req, err := http.NewRequest(c.Request.Method, proxyURL.String(), c.Request.Body)
-		if err != nil {
-			p.logger.Errorf("Failed to create proxy request: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
-			return
-		}
-		req.Header = c.Request.Header
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			p.logger.Errorf("Failed to send proxy request: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send proxy request"})
-			return
-		}
-		defer resp.Body.Close()
-
-		p.logger.Info("Request proxied", "url", proxyURL.String(), "statusCode", resp.StatusCode)
-		for key, values := range resp.Header {
-			for _, value := range values {
-				c.Writer.Header().Add(key, value)
-			}
-		}
-		c.Status(resp.StatusCode)
-		_, err = io.Copy(c.Writer, resp.Body)
-		if err != nil {
-			p.logger.Errorf("Failed to copy response body: %v", err)
-			return
-		}
-	}
 }
 
 func (p *Proxy) Run(addr string) {
