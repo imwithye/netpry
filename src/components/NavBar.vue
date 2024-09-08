6a41cd6d0476e00c520431c62893db14327ad8e3
Yiwei Gong <imwithye@gmail.com>
2024-09-08 22:24:08 +0800

    add setting dialog

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-[32px] flex justify-between items-center px-4 py-1 font-mono">
+  <div class="w-full h-[48px] flex justify-between items-center px-4 py-1 font-mono">
     <div class="w-[128px]"></div>
     <div class="flex justify-between items-center">
       <el-icon class="mr-2">
@@ -20,21 +20,24 @@
           </el-icon>
         </template>
       </el-switch>
-      <el-icon>
-        <Setting />
-      </el-icon>
+      <el-button :icon="Setting" text @click="() => (settingDialogVisible = true)" />
     </div>
   </div>
+  <SettingDialog :visible="settingDialogVisible" @close="() => (settingDialogVisible = false)" />
 </template>
 
 <script setup lang="ts">
 import { useDark, useToggle } from '@vueuse/core'
 import { Histogram, Sunny, Moon, Setting } from '@element-plus/icons-vue'
+import SettingDialog from './SettingDialog.vue'
+import { ref } from 'vue'
 
 const isDark = useDark({
   disableTransition: false
 })
 const toggleDark = useToggle(isDark)
+
+const settingDialogVisible = ref(false)
 </script>
 
 <style scoped>
---
+++ src/components/SettingDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-dialog :model-value="visible" title="Setting" width="400" @close="() => $emit('close')">
+    <el-form label-width="auto">
+      <el-form-item label="Ingress Endpoint">
+        <el-input />
+      </el-form-item>
+      <el-form-item label="Backend Endpoint">
+        <el-input />
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="() => $emit('close')">Cancel</el-button>
+        <el-button type="primary">Confirm</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  visible: {
+    type: Boolean
+  }
+})
+</script>
