e7d967dcb7764eec512d4d7f353cec201855a882
Yiwei Gong <imwithye@gmail.com>
2024-09-08 21:40:00 +0800

    fix navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="w-full h-[32px] flex justify-between items-center px-4 py-1 font-mono">
-    <div></div>
-    <div>
+    <div class="w-[128px]"></div>
+    <div class="flex justify-between items-center">
       <el-icon class="mr-2">
         <Histogram />
       </el-icon>
-      <span>netpry - network inspection</span>
+      <span>netpry - network inspector</span>
     </div>
-    <div>
-      <el-switch class="theme-switch" v-model="isDark" @change="toggleDark">
+    <div class="w-[128px] flex justify-end items-center">
+      <el-switch class="theme-switch mr-2" v-model="isDark" @change="toggleDark">
         <template #active-action>
           <el-icon>
             <Moon class="text-gray-200" />
@@ -20,13 +20,16 @@
           </el-icon>
         </template>
       </el-switch>
+      <el-icon>
+        <Setting />
+      </el-icon>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useDark, useToggle } from '@vueuse/core'
-import { Histogram, Sunny, Moon } from '@element-plus/icons-vue'
+import { Histogram, Sunny, Moon, Setting } from '@element-plus/icons-vue'
 
 const isDark = useDark({
   disableTransition: false
