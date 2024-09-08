721f9295bf3e01acb9fa5859c03f8ae13a12d7a5
Yiwei Gong <imwithye@gmail.com>
2024-09-08 21:30:29 +0800

    set row active color

--- src/components/RequestList.vue
+++ src/components/RequestList.vue
@@ -7,7 +7,7 @@
       class="w-full"
       height="100%"
       :data="requestDetailsStore.requestDetailsList"
-      row-class-name="cursor-pointer"
+      :row-class-name="tableRowClassName"
       @row-click="(row: RequestDetails) => setActivatedRequestDetails(row)"
     >
       <el-table-column prop="method" label="Method" width="80" min-width="80">
@@ -33,11 +33,25 @@
 import { RequestDetails, useRequestDetailsStore } from '../store/request_details_store.ts'
 import MethodTag from './MethodTag.vue'
 import URI from './URI.vue'
+import { computed } from 'vue'
 
 const requestDetailsStore = useRequestDetailsStore()
+const requestDetails = computed(() => requestDetailsStore.activatedRequestDetails)
 
 const setActivatedRequestDetails = (row: RequestDetails) => {
   const req: RequestDetails = JSON.parse(JSON.stringify(row))
   requestDetailsStore.setActiveRequestDetails(req)
 }
+
+const tableRowClassName = ({ row }: { row: RequestDetails }) => {
+  if (requestDetails.value && requestDetails.value.uri == row.uri)
+    return 'cursor-pointer active-row'
+  return 'cursor-pointer'
+}
 </script>
+
+<style scoped>
+:deep(.active-row) {
+  background-color: var(--el-table-current-row-bg-color);
+}
+</style>
